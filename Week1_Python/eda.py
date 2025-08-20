af9e12ef56d8257f5dabb7ca2caa11f8f23008ac
khrisvie <khr.grynyuk@gmail.com>
2025-08-20 19:45:26 +0300

    Add function view_entry(). Add iterations to add_entry()

--- Week1_Python/eda.py
+++ Week1_Python/eda.py
@@ -1,19 +1,59 @@
+#%%
 import csv
+import pandas as pd
+from IPython.display import display
+
 
 def add_entry():
     with open("data.csv", mode="a", newline="") as data_csv:
-        fieldnames = ["Date","Category","Description","Amount"]
+        fieldnames = ["Date","Category","Description","Amount","Currency"]
         writer = csv.DictWriter(data_csv, fieldnames=fieldnames)
 
-        date = input("Enter date: ")
-        category = input("Enter category: ")
-        description = input("Enter description: ")
-        amount = input("Enter amount: ")
+        while True:
+            date = input("Enter date: ")
+            category = input("Enter category: ")
+            description = input("Enter description: ")
+            amount = input("Enter amount: ")
+            amount_ = ''.join(filter(lambda c: c.isdigit() or c == '.', amount))
+            currency = ''.join(filter(lambda c: not c.isdigit() and c != '.', amount))
+        
+            if not currency:
+                currency = input("Enter currency symbol: ")
 
-        writer.writerow({"Date": date,
-                         "Category": category,
-                         "Description": description,
-                         "Amount": amount})
-    print("Added!")
+            amount_ = float(amount_)
 
-add_entry()
+            writer.writerow({
+                "Date": date,
+                "Category": category,
+                "Description": description,
+                "Amount": amount_,
+                "Currency": currency
+              })
+            print("Added!")
+            more = input("Add another entry? (y/n): ").strip().lower()
+            if more != "y":
+                break
+
+#add_entry()
+
+def view_entries():
+    df = pd.read_csv("data.csv")
+    
+    if df.empty:
+        print("No entries to display.")
+        return
+    
+    df["Date"] = pd.to_datetime(df["Date"], dayfirst=True, errors="coerce")
+    df = df.sort_values("Date")
+    df["Date"] = df["Date"].dt.strftime("%d.%m.%Y")
+   
+    df["Amount"] = pd.to_numeric(df["Amount"], errors="coerce")
+    df["Amount"] = df["Amount"].apply(lambda x: f"{x:,.2f}") + " " + df["Currency"]
+   
+    df["Currency"] = df["Currency"].astype(str).fillna("")
+     
+    display_df = df[["Date", "Category", "Description", "Amount"]]
+    
+    display(display_df)
+
+view_entries()
